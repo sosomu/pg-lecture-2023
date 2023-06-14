46251966fc35eebc61cb0894ae950c13146a25ff
sosomu <136149234+sosomu@users.noreply.github.com>
2023-06-14 15:19:21 +0900

    feat: 画像が読み込まれたときに裏向きのカードを表示

--- index.mjs
+++ index.mjs
@@ -1,6 +1,7 @@
 import { shuffle } from './js/utils.mjs';
 
-
+const maxPair = 9;
+const cardImage = [];
 
 // HTML のパース完了後まで initialize の実行を遅延させるための処理
 if (document.readyState === 'loading') {
@@ -10,5 +11,25 @@ if (document.readyState === 'loading') {
 }
 
 function initialize() {
-    
+    const arr =[];
+
+    for (let i = 0; i < maxPair; i++){
+        arr.push(i);
+        arr.push(i);
+    }
+
+    shuffle(arr);
+
+    const panel = document.getElementById('panel');
+    for (let i = 0; i < maxPair * 2; i++){
+        const div = document.createElement('div');
+        div.classList.add('card','back');
+        div.dataset.index = i;
+        div.dataset.number = arr[i];
+        panel.appendChild(div);
+
+        const img = new Image();
+        img.src = 'images/card'+ (arr[i] + 1) + '.png'
+        cardImage.push(img);
+    }
 }
